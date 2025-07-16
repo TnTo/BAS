767543c9679c4a69d5abd47300eadbf6bd08a4fc
margherita 'TnTo' redigonda <m AT orsorosso DOT net>
2025-07-16 17:08:17 +0200

    to fix FK debt

--- 03AB_Markup.R
+++ 03AB_Markup.R
@@ -182,8 +182,8 @@ sample.vec <- function(x, ...) x[sample.int(length(x), ...)]
         for (t in 2:TMAX) {
             muC[t, ] <- muC[t - 1, ] * (1 + theta * (cuC[t - 1, ] - cuT) / cuT)
             muK[t, ] <- muK[t - 1, ] * (1 + theta * (cuK[t - 1, ] - cuT) / cuT)
-            pC[t] <- (1 + muC[t, ]) * (W0 + pKC[t - 1] * dK) / betaC # price
-            pK[t] <- (1 + muK[t, ]) * (W0 + pK[t - 1] * dK) / betaK # price
+            pC[t, ] <- (1 + muC[t, ]) * (W0 + pKC[t - 1] * dK) / betaC # price
+            pK[t, ] <- (1 + muK[t, ]) * (W0 + pK[t - 1] * dK) / betaK # price
             pC[t, ] <- ifelse(is.na(pC[t, ]), pC[t - 1, ], pC[t, ])
             HpC[t, ] <- (1 + tC) * pC[t, ] # price after VAT (Hs price)
 
